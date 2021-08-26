540f00c5744f015a23cb36a932f165f1bbef64ac
Jerry Ng <ngshiheng@gmail.com>
2021-08-26 09:57:47 +0800

    Add primary object APIs to create and remove vehicle

---
+++ api/vehicle.py
@@ -0,0 +1,21 @@
+from lib.database import VehicleType, add_vehicle, remove_vehicle
+
+from api.event import post_event
+
+
+def record_vehicle_entry(vehicle_type: VehicleType, number_plate: str, epoch_timestamp: str) -> None:
+    """
+    API to create Vehicle object in database.
+    """
+
+    event = add_vehicle(vehicle_type, number_plate, int(epoch_timestamp))
+    post_event("vehicle_enter", event)
+
+
+def record_vehicle_exit(number_plate: str, epoch_timestamp: str) -> None:
+    """
+    API to remove Vehicle object from database.
+    """
+
+    event = remove_vehicle(number_plate, int(epoch_timestamp))
+    post_event("vehicle_exit", event)
